833f15e40be6d4830471dc3b00dde76f7d468fd2
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-25 11:34:27 -0300

    feat(auth): authorization with github and next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,33 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
       <button 
         type="button"
         className={styles.signInButton}
       >
         <FaGithub color="#04D361" />
-        Diego Fernandes
-        <FiX className={styles.closeIcon} color="#737380" />
+          {session.user.name}
+        <FiX 
+          className={styles.closeIcon} 
+          color="#737380" 
+          onClick={() => signOut()}
+        />
       </button>
   ) : (
     <button 
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
       >
         <FaGithub color="#EBA417" />
-        Sign in with github
+        Sign in with Github
       </button>
   )
 }
