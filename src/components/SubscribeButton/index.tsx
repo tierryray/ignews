b71396a09f598d169e473879f9d42318fa9542c7
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-29 17:31:38 -0300

    feat(post-preview): create preview post

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -3,6 +3,7 @@ import { signIn, useSession } from 'next-auth/client';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -10,10 +11,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session?.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
